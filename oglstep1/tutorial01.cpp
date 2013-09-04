9ff9b53265df67cf1ccb3056bed13a75849734f4
sweetdark <qiujunwei1@gmail.com>
2013-09-04 22:25:21 +0800

    update

--- oglstep1/tutorial01.cpp
+++ oglstep1/tutorial01.cpp
@@ -1,20 +1,57 @@
 #include <gl/glut.h>
 #include <gl/GL.h>
 
+static GLfloat x1 = 0.0f;
+static GLfloat y1 = 0.0f;
+
+static GLfloat rsize = 25.0f;
+static GLfloat xstep = 1.0f;
+static GLfloat ystep = 1.0f;
+
+static GLsizei xWindow = 0;
+static GLsizei yWindow = 0;
+
 static void RenderScenceCB() {
 	
 	glClear(GL_COLOR_BUFFER_BIT);
   glColor3f(0.0f, 1.0f, 0.0f);
-  glRectf(-25.0f, 25.0f, 25.0f, -25.0f);
-  glFlush();
+  glRectf(x1, y1, x1 + rsize, y1 - rsize);
+  glutSwapBuffers();
 }
 
 static void CreateWindow() {
-	glutInitWindowPosition(200, 200);
-	glutInitWindowSize(400, 400);
+	//glutInitWindowPosition(200, 200);
+	glutInitWindowSize(200, 200);
 	glutCreateWindow("opengl step 1");
 }
 
+static void TimerFunc(int value)
+{
+ /* if (x1 > xWindow - rsize || x1 < -xWindow )
+  {
+    xstep = -xstep;
+  }
+
+  if (y1 > yWindow || y1 < -yWindow + rsize)
+  {
+    ystep = -ystep;
+  }
+
+  x1 += xstep;
+  y1 += ystep;*/
+
+  //if (x1 > xWindow - rsize + xstep)
+  //{
+  //  x1 = xWindow - rsize - 1;
+  //}
+  //else if (x1 < -xWindow)
+  //{
+  //  x1 = -xWindow + 1;
+  //}
+
+  RenderScenceCB();
+  glutTimerFunc(33, TimerFunc, 0);
+}
 static void ChangeSize(GLsizei w, GLsizei h)
 {
   glViewport(0, 0, w, h);
@@ -24,6 +61,19 @@ static void ChangeSize(GLsizei w, GLsizei h)
   {
     h = 1;
   }
+
+  if (rsize > w)
+  {
+    rsize = w/2;
+  }
+
+  if (rsize > h)
+  {
+    rsize = h/2;
+  }
+
+  xWindow = w/2;
+  yWindow = h/2;
   GLfloat aspectRatio = (GLfloat)w / (GLfloat)h;
   if (w <= h)
   {
@@ -38,10 +88,11 @@ static void ChangeSize(GLsizei w, GLsizei h)
 }
 int main(int args, char *argc[]) {
 	glutInit(&args, argc);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	CreateWindow();
 	glutDisplayFunc(RenderScenceCB);
   glutReshapeFunc(ChangeSize);
+  glutTimerFunc(33, TimerFunc, 0);
 	glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 	glutMainLoop();
 
