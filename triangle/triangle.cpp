b15c688f88b20026967f90b682bd9bb24887a60f
sweetdark <qiujunwei1@gmail.com>
2013-09-12 18:01:34 +0800

    update

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,31 +1,106 @@
 #include "headers.h"
 
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+static bool bDepth = false;
+static bool bCull = false;
+static bool bOutLine = false;
 
+static void PopMenu(int key)
+{
+  if (key == 1)
+  {
+    bDepth = !bDepth;
+  }
+
+  if (key == 2)
+  {
+    bCull = !bCull;
+  }
+
+  if (key == 3)
+  {
+    bOutLine = !bOutLine;
+  }
+    
+  glutPostRedisplay();
+}
 static void RenderScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	GLfloat x, y, z, angle;
+  bool color;
 	glColor3f(1.0f, 1.0f, 0.0f);
+
+  if (bDepth)
+  {
+    glEnable(GL_DEPTH_TEST);
+  }
+  else
+  {
+    glDisable(GL_DEPTH_TEST);
+  }
+
+  if (bCull)
+  {
+    glEnable(GL_CULL_FACE);
+  }
+  else
+  {
+    glDisable(GL_CULL_FACE);
+  }
+  
+  if (bOutLine)
+  {
+    glPolygonMode(GL_BACK, GL_LINE) ;
+  }
+  else
+  {
+    glPolygonMode(GL_BACK, GL_FILL);
+  }
+  
+
+  glPushMatrix();
+  glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+  glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+
+  glBegin(GL_TRIANGLE_FAN);
+    glVertex3f(0.0f, 0.0f, 70.0f);
+
+    for (angle = 0.0f; angle <= (2 * GL_PI); angle += (GL_PI / 8))
+    {
+  		x = 50.0f * sin(angle);
+  		y = 50.0f * cos(angle);
+  		color = !color;
+  	
+      glVertex3f(x, y, 0.0f);
+  		if(color)
+  			glColor3f(1.0f, 0.0f, 0.0f);
+  		else
+  			glColor3f(0.0f, 1.0f, 1.0f);
+    }
+  glEnd();
+
 	glBegin(GL_TRIANGLE_FAN);
-	glVertex2f(0.0f, 0.0f);
-	bool color;
+  	glVertex2f(0.0f, 0.0f);
 
-	for (angle = 0.0f; angle <= (2 * GL_PI); angle += (GL_PI / 8))
-	{
-		x = 50.0f * sin(angle);
-		y = 50.0f * cos(angle);
-		color = !color;
-	
-		glVertex2f(x, y);
+  	for (angle = 0.0f; angle <= (2 * GL_PI); angle += (GL_PI / 8))
+  	{
+  		x = 50.0f * sin(angle);
+  		y = 50.0f * cos(angle);
+  		color = !color;
+  	
+  		glVertex2f(x, y);
 
-		if(color)
-			glColor3f(1.0f, 0.0f, 0.0f);
-		else
-			glColor3f(0.0f, 1.0f, 1.0f);
-	}
+  		if(color)
+  			glColor3f(1.0f, 0.0f, 0.0f);
+  		else
+  			glColor3f(0.0f, 1.0f, 1.0f);
+  	}
 	glEnd();
 
+  glPopMatrix();
 	glutSwapBuffers();
 }
 
@@ -34,6 +109,7 @@ static void SetupRC()
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glEnable(GL_CW);
 	glShadeModel(GL_FLAT);
+  glCullFace(GL_BACK);
 }
 
 
@@ -65,6 +141,29 @@ void ChangeSize(GLsizei w, GLsizei h)
 	glLoadIdentity();
 }
 
+static void SpecialKey(int key, int x, int y)
+{
+  if (key == GLUT_KEY_UP)
+  {
+    xRot += 2.0f;
+  }
+  if (key == GLUT_KEY_DOWN)
+  {
+    xRot -= 2.0f;
+  }
+  if (key == GLUT_KEY_LEFT)
+  {
+    yRot += 2.0f;
+  }
+  if (key == GLUT_KEY_RIGHT)
+  {
+    yRot -= 2.0f;
+  }
+
+  xRot = (GLint)xRot % 360;
+  yRot = (GLint)yRot % 360;
+  glutPostRedisplay();
+}
 int main(int args, char **argv)
 {
 	glutInit(&args, argv);
@@ -73,7 +172,13 @@ int main(int args, char **argv)
 	glutInitWindowSize(200, 200);
 	glutCreateWindow("draw points");
   SetupRC();
-	glutDisplayFunc(RenderSences);	
+
+  glutCreateMenu(PopMenu);
+  glutAddMenuEntry("深度缓冲", 1);
+  glutAddMenuEntry("裁剪面", 2);
+  glutAddMenuEntry("背面画线", 3);
+  glutAttachMenu(GLUT_RIGHT_BUTTON);
+	glutDisplayFunc(RenderScene);	
 	glutReshapeFunc(ChangeSize);
   glutSpecialFunc(SpecialKey);
 	glutMainLoop();
